3e914e479781eed44ea4dcb26b43f948a4c11c01
Jin Tang <jin.tang@vmob.com>
2017-05-01 10:16:32 +1200

    csv reader

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -1,15 +1,34 @@
 import mysql.connector
+class ConnectionError(Exception):
+	pass
+
+class CredentialsError(Exception):
+	pass
+
+class SQLError(Exception):
+	pass
+
 class UseDatabase:
 	def __init__(self, config:dict)-> None:
 		self.configuration = config
 
 	def __enter__(self)->'Cursor':
-		self.Conn=mysql.connector.connect(**self.configuration)
-		self.Cursor=self.Conn.cursor()
-		return self.Cursor
+		try:
+			self.Conn=mysql.connector.connect(**self.configuration)
+			self.Cursor=self.Conn.cursor()
+			return self.Cursor
+		except mysql.connector.errors.InterfaceError as e:
+			raise ConnectionError(e)
+		except mysql.connector.errors.ProgrammingError as e:
+			raise CredentialsError(e)
+
 
 	def __exit__(self, exc_type, exc_value, exc_trace) ->None:
 		self.Conn.commit()
 		self.Cursor.close()
 		self.Conn.close()
+		if(exc_type is mysql.connector.errors.ProgrammingError):
+			raise SQLError(exc_value)
+		elif exc_type:
+			raise exc_type(exc_value)
 
--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,24 +1,14 @@
-from flask import Flask, render_template, request, redirect, escape, session
+from flask import Flask, render_template, request, redirect, escape, session, copy_current_request_context
 from vsearch import search4letter
-from DBcm import UseDatabase
+from DBcm import UseDatabase, ConnectionError, CredentialsError, SQLError
 from checker import check_logged_in
+from threading import Thread
+import time
 app = Flask(__name__)
 app.config['dbconfig'] = {'host':'127.0.0.1',
 				'user':'vsearch',
 				'password':'vsearchpasswd',
 				'database':'vsearchlogDB',}
-def log_request(req:'flask_request', res: str) ->None:
-
-	###conn = mysql.connector.connect(**dbconfig)
-	###cursor = conn.cursor()
-	with UseDatabase(app.config['dbconfig']) as curor:
-		_SQL = """ insert into log (phrase,letters,ip,browser_string,results) values (%s,%s,%s,%s,%s) """
-		cursor.execute(_SQL,(req.form['phrase'],req.form['letters'],req.remote_addr,req.user_agent.browser,res,))
-	###conn.commit()
-	###cursor.close()
-	###conn.close()
-	###with open('vsearch.log','a') as log:
-	###	print(req.form, req.remote_addr,req.user_agent, res, file=log, sep='|')
 
 @app.route('/')
 def hello () -> '302':
@@ -26,12 +16,23 @@ def hello () -> '302':
 
 @app.route('/search4', methods=['post'])
 def do_search() -> 'html':
-    #return str(search4letter('life, the universe, and everything', 'eiru,!'))
-    phrase = request.form['phrase']
-    letters = request.form['letters']
-    results = str(search4letter(phrase,letters))
-    log_request(request,results)
-    return render_template('results.html',
+	@copy_current_request_context
+	def log_request(req:'flask_request', res: str) ->None:
+		time.sleep(15)
+		with UseDatabase(app.config['dbconfig']) as curor:
+			_SQL = """ insert into log (phrase,letters,ip,browser_string,results) values (%s,%s,%s,%s,%s) """
+			cursor.execute(_SQL,(req.form['phrase'],req.form['letters'],req.remote_addr,req.user_agent.browser,res,))
+
+	phrase = request.form['phrase']
+	letters = request.form['letters']
+	results = str(search4letter(phrase,letters))
+	try:
+		t= Thread(target=log_request,args=(request,results))
+		t.start()
+	except Exception as e:
+		print('***Logging failed with error: ', str(e))
+
+	return render_template('results.html',
     						the_title='Welcome to search4letters on the web!',
     						the_phrase= phrase,
     						the_letters = letters,
@@ -45,11 +46,27 @@ def entry_page() -> 'html':
 @app.route('/viewlog')
 @check_logged_in
 def view_the_log() ->'html':
+	try:
+		with UseDatabase(app.config['dbconfig']) as cursor:
+			_SQL= """select phrase, letters, ip, browser_string, results from log"""
+			cursor.execute(_SQL)
+			contents = cursor.fetchall()
+			titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
+		return render_template('viewlog.html',
+								the_title='View Log',
+								the_row_titles=titles,
+								the_data=contents,)
+	except ConnectionError as e:
+		print('Is your database switched on? Error: ', str(e))
+	except CredentialsError as e:
+		print('User-id/Password issues. Error: ', str(e))
+	except SQLError as e:
+		print ('Is your query correct? Error: ', str(e))
+	except Exception as e:
+		print('Someting went wrong: ', str(e))
 
-	with UseDatabase(app.config['dbconfig']) as cursor:
-		_SQL= """select phrase, letters, ip, browser_string, results from log"""
-		cursor.execute(_SQL)
-		contents = cursor.fetchall()
+	return 'Error'
+	
 	##contents=[]
 	##with open('vsearch.log') as log:
 	##	for line in log:
@@ -57,11 +74,7 @@ def view_the_log() ->'html':
 	##		for item in line.split('|'):
 	##			contents[-1].append(escape(item))
 	
-	titles = ('Phrase', 'Letters', 'Remote_addr', 'User_agent', 'Results')
-	return render_template('viewlog.html',
-							the_title='View Log',
-							the_row_titles=titles,
-							the_data=contents,)
+	
 
 @app.route('/login')
 def do_login() ->str:
